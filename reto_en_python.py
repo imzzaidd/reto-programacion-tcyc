1d7224de682ed133f9fd02c6e55bfd6032eb18c4
Zaid García <angieillumi@gmail.com>
2024-09-09 23:22:20 -0600

    feat: Imprime 2037 en Python

--- reto_en_python.py
+++ reto_en_python.py
@@ -1,23 +1,45 @@
 import re
+file_path = 'LANGUAGE.TXT'
 
-def extract_languages(file_path):
-    languages = []
-    regex = re.compile(r"^[A-Za-z][A-Za-z0-9+\-\s]*$")
+try:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        file_contents = file.read()
     
-    with open(file_path, 'r') as file:
-        for line in file:
-            line = line.strip()
-            if regex.match(line) and 1 < len(line) < 40:
-                languages.append(line)
+    # Expresión regular 
+    pattern = re.compile(r'^[A-Za-z0-9_\-\.+\/*&|~`<>?!@#\^()\[\]{}]+(?:\s*\([^)]*\))?\s*-', re.MULTILINE)
     
-    # Sort the languages alphabetically
-    languages.sort()
-
-    # Print the total number of languages and the names
-    print(f"Total lenguajes: {len(languages)}")
+    #Coincidencias
+    matches = pattern.findall(file_contents)
+    
+    languages = set()
+    
+    # Extrae los nombres de lenguajes y aplica filtros
+    for match in matches:
+        language_name = match.split(' -')[0].strip()
+        
+        #1
+        unwanted_patterns = [r'^p\.', r'^pp\.', r'^[0-9]', r'^[a-z]{1,2}$', r'^x86', r'^[A-Za-z]$', r'^\s*$', r'^\d{2,}[^a-zA-Z\d]', r'^\s*\([^)]*\)\s*$']
+        is_valid = True
+        
+        for pattern in unwanted_patterns:
+            if re.match(pattern, language_name):
+                is_valid = False
+                break
+        
+        #2
+        if is_valid:
+            languages.add(language_name)
+    
+    #Orden alfabético
+    languages = sorted(languages)
+    
+    # Imprime los lenguajes en orden alfabético
     for language in languages:
         print(language)
-
-# Usage
-file_path = "LANGUAGE.TXT"
-extract_languages(file_path)
+    
+    # Imprime el total de lenguajes encontrados
+    print("\nTotal de lenguajes encontrados:", len(languages))
+    
+except Exception as e:
+    #Excepción
+    print("Error al leer el archivo:", str(e))

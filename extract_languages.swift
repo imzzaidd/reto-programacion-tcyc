409cd953887fefd109fe5b7a7ae0de0e90ac5327
Zaid García <angieillumi@gmail.com>
2024-09-09 22:50:00 -0600

    feat: 2037 lenguajes en Swift

--- extract_languages.swift
+++ extract_languages.swift
@@ -1,28 +1,22 @@
 import Foundation
-
-// Ruta al archivo .txt
 let filePath = "LANGUAGE.TXT"
 
 do {
-    // Lee el contenido del archivo como una cadena
     let fileContents = try String(contentsOfFile: filePath, encoding: .utf8)
-    
-    // Expresión regular ampliada para capturar una gama aún más amplia de nombres de lenguajes
     let regex = try NSRegularExpression(pattern: "^[A-Za-z0-9_\\-\\.\\+\\*/&|~`<>?!@#\\^\\(\\)\\[\\]{}]+(?:\\s*\\([^\\)]+\\))?\\s*-", options: .anchorsMatchLines)
     
-    // Encuentra todas las coincidencias en el archivo
+    //Coincidencias en el archivo
     let matches = regex.matches(in: fileContents, options: [], range: NSRange(location: 0, length: fileContents.utf16.count))
-    
     var languages = [String]()
     
-    // Extrae los nombres de lenguajes coincidentes y los añade a la lista, evitando duplicados y filtrando palabras no deseadas
+    // Extrae los nombres de lenguajes 
     for match in matches {
         if let range = Range(match.range, in: fileContents) {
             let languageName = String(fileContents[range])
                 .components(separatedBy: " -").first!  // Elimina la descripción que sigue al nombre del lenguaje
                 .trimmingCharacters(in: .whitespacesAndNewlines)  // Limpia espacios en blanco innecesarios
             
-            // Filtra nombres no deseados (ajustar patrones según sea necesario)
+            // Filtra nombres no deseados
             let unwantedPatterns = ["^p\\.", "^pp\\.", "^[0-9]", "^[a-z]{1,2}$", "^x86", "^[A-Za-z]$", "^\\s*$", "^\\d{2,}[^a-zA-Z\\d]", "^(?:\\s*\\([^\\)]+\\))?$"]
             var isValid = true
             for pattern in unwantedPatterns {
@@ -32,7 +26,6 @@ do {
                     break
                 }
             }
-            
             // Agregar solo lenguajes válidos y evitar duplicados
             if isValid && !languages.contains(languageName) {
                 languages.append(languageName)
@@ -40,18 +33,14 @@ do {
         }
     }
     
-    // Ordena los lenguajes en orden alfabético
+    // Orden e impresión de datos
     languages.sort()
-    
-    // Imprime los lenguajes en orden alfabético
     for language in languages {
         print(language)
     }
-    
-    // Imprime el total de lenguajes encontrados
     print("\nTotal de lenguajes encontrados: \(languages.count)")
     
 } catch {
-    // Manejo de errores al leer el archivo
+    // Excepciones
     print("Error al leer el archivo: \(error.localizedDescription)")
 }
